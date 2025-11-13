b6f62b18c33d607f85cf345c3d9b4628adbc6557
Ashuthosh0 <pvgar09@gmail.com>
2025-11-13 21:30:01 +0530

    Add lab template two

---
+++ lab-2/settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ZMSeqgy3FHRUVWorU3mho8Md',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'crud',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
